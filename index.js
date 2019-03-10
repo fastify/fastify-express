133a197c458c8a7a815ae344e55be6aab339ef53
delvedor <tommydelved@gmail.com>
2019-03-10 17:54:25 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const fp = require('fastify-plugin')
+const Express = require('express')
+
+function expressPlugin (fastify, options, next) {
+  const express = Express()
+
+  // TODO: we should use decorate, but `use` is already
+  // a public API of Fastify. In Fastify v3 it will be deprecated,
+  // so we will able to use `decorate`
+  fastify.use = use
+  fastify.addHook('onRequest', runConnect)
+
+  function use (fn) {
+    express.use(fn)
+    return this
+  }
+
+  function runConnect (req, reply, next) {
+    express(req.raw, reply.res, next)
+  }
+
+  next()
+}
+
+module.exports = fp(expressPlugin, {
+  fastify: '>=2.x',
+  name: 'fastify-express'
+})
