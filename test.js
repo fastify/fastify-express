7df01c8392bffc468e5b673ee5217dce78169743
delvedor <tommydelved@gmail.com>
2019-03-11 14:13:59 +0100

    Updated test

--- test.js
+++ test.js
@@ -4,6 +4,8 @@ const { test } = require('tap')
 const Fastify = require('fastify')
 const sget = require('simple-get').concat
 const cors = require('cors')
+const passport = require('passport')
+const Strategy = require('passport-http-bearer').Strategy
 
 const expressPlugin = require('./index')
 
@@ -28,6 +30,74 @@ test('Should support connect style middlewares', t => {
         fastify.close()
       })
     })
+})
+
+test('Should support complex middlewares', t => {
+  t.plan(4)
+
+  const fastify = Fastify()
+
+  passport.use(new Strategy((token, cb) => {
+    t.strictEqual(token, '123456789')
+    cb(null, { token })
+  }))
+
+  fastify
+    .register(expressPlugin)
+    .after(() => fastify.use(passport.authenticate('bearer', { session: false })))
+    .get('/', (req, reply) => {
+      t.deepEqual(req.raw.user, { token: '123456789' })
+      reply.send('ok')
+    })
+    .listen(0, (err, address) => {
+      t.error(err)
+      sget({
+        method: 'GET',
+        url: address,
+        headers: {
+          authorization: 'Bearer 123456789'
+        }
+      }, (err, res, data) => {
+        t.error(err)
+        fastify.close()
+      })
+    })
+})
+
+test('Encapsulation support', t => {
+  t.plan(2)
+
+  const fastify = Fastify()
+
+  fastify.register((instance, opts, next) => {
+    instance.register(expressPlugin)
+      .after(() => instance.use(middleware))
+
+    instance.get('/plugin', (req, reply) => {
+      reply.send('ok')
+    })
+
+    next()
+  })
+
+  fastify.get('/', (req, reply) => {
+    reply.send('ok')
+  })
+
+  fastify.listen(0, (err, address) => {
+    t.error(err)
+    sget({
+      method: 'GET',
+      url: address
+    }, (err, res, data) => {
+      t.error(err)
+      fastify.close()
+    })
+  })
+
+  function middleware (req, res, next) {
+    t.fail('Shuld not be called')
+  }
 })
 
 test('Middleware chain', t => {
@@ -71,7 +141,7 @@ test('Middleware chain', t => {
   }
 })
 
-test('Middleware chain (with errors)', t => {
+test('Middleware chain (with errors) / 1', t => {
   t.plan(8)
 
   const order = [1, 2, 3]
@@ -112,6 +182,53 @@ test('Middleware chain (with errors)', t => {
     t.is(err.message, 'middleware2')
     t.strictEqual(order.shift(), 3)
     next(new Error('kaboom'))
+  }
+})
+
+test('Middleware chain (with errors) / 2', t => {
+  t.plan(7)
+
+  const order = [1, 2]
+  const fastify = Fastify()
+
+  fastify.setErrorHandler((err, req, reply) => {
+    t.is(err.message, 'middleware2')
+    reply.send(err)
+  })
+
+  fastify
+    .register(expressPlugin)
+    .after(() => {
+      fastify
+        .use(middleware1)
+        .use(middleware2)
+        .use(middleware3)
+    })
+    .listen(0, (err, address) => {
+      t.error(err)
+      sget({
+        method: 'GET',
+        url: address
+      }, (err, res, data) => {
+        t.error(err)
+        t.strictEqual(res.statusCode, 500)
+        fastify.close()
+      })
+    })
+
+  function middleware1 (req, res, next) {
+    t.strictEqual(order.shift(), 1)
+    next(new Error('middleware1'))
+  }
+
+  function middleware2 (err, req, res, next) {
+    t.is(err.message, 'middleware1')
+    t.strictEqual(order.shift(), 2)
+    next(new Error('middleware2'))
+  }
+
+  function middleware3 (req, res, next) {
+    t.fail('We should not be here')
   }
 })
 
