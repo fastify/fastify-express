d6dd9dc1b252a10f2a148164215b78a01782cbd1
delvedor <tommydelved@gmail.com>
2019-12-24 11:49:24 +0100

    Fixed encapsulation

--- index.js
+++ index.js
@@ -2,33 +2,66 @@
 
 const fp = require('fastify-plugin')
 const Express = require('express')
+// const kMiddlewares = Symbol('fastify-express-middlewares')
+// const kExpress = Symbol('fastify-express-instance')
 
 function expressPlugin (fastify, options, next) {
-  const express = Express()
-
   // TODO: we should use decorate, but `use` is already
   // a public API of Fastify. In Fastify v3 it will be deprecated,
   // so we will able to use `decorate`
   fastify.use = use
-  fastify.addHook('onRequest', runConnect)
+  fastify
+    .decorate('__middlewares', [])
+    .decorate('__express', Express())
+    .addHook('onRoute', onRoute)
+    .addHook('onSend', runOnSend)
+    .addHook('onRegister', onRegister)
 
   function use (path, fn) {
+    this.__middlewares.push([path, fn])
     if (fn == null) {
-      express.use(path)
+      this.__express.use(path)
     } else {
-      express.use(path, fn)
+      this.__express.use(path, fn)
     }
     return this
   }
 
   function runConnect (req, reply, next) {
-    express(req.raw, reply.res, next)
+    if (this.__middlewares.length > 0) {
+      this.__express(req.raw, reply.res, next)
+    } else {
+      next()
+    }
+  }
+
+  function onRoute (opts) {
+    opts.onRequest = opts.onRequest || []
+    if (!Array.isArray(opts.onRequest)) {
+      opts.onRequest = [opts.onRequest]
+    }
+    opts.onRequest.push(runConnect)
+  }
+
+  function runOnSend (req, reply, payload, next) {
+    reply.res.removeHeader('x-powered-by')
+    next()
+  }
+
+  function onRegister (instance) {
+    const middlewares = instance.__middlewares.slice()
+    instance.__middlewares = []
+    instance.__express = Express()
+    instance.use = use
+    for (const middleware of middlewares) {
+      instance.use(...middleware)
+    }
   }
 
   next()
 }
 
 module.exports = fp(expressPlugin, {
-  fastify: '>=2.x',
+  fastify: '>=3.0.0',
   name: 'fastify-express'
 })
