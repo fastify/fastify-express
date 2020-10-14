4cd9e228b87fc54a0faa5cab45fdc53f14b2acdb
Simen Bekkhus <sbekkhus91@gmail.com>
2020-10-14 17:38:05 +0200

    feat: use decorator and add `express` to instance (#8)
    * feat: use decorator and add `express` to instance
    
    * readme

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,14 @@
+import { Application } from 'express'
 import { FastifyPluginCallback } from 'fastify'
+
+declare module "fastify" {
+  interface FastifyInstance {
+    /**
+     * Express application instance
+     */
+    express: Application
+  }
+}
 
 export const fastifyExpress: FastifyPluginCallback
 
--- test/application.test.js
+++ test/application.test.js
@@ -115,3 +115,31 @@ test('Should remove x-powered-by header', t => {
     })
   })
 })
+
+test('Should expose the express app on the fastify instance', t => {
+  t.plan(3)
+  const fastify = Fastify()
+  t.teardown(fastify.close)
+
+  const router = Express.Router()
+
+  router.get('/', (req, res) => {
+    res.status(201)
+    res.json({ hello: 'world' })
+  })
+
+  fastify
+    .register(expressPlugin)
+    .after(() => { fastify.use(router) })
+
+  fastify.listen(0, (err, address) => {
+    t.error(err)
+    sget({
+      method: 'GET',
+      url: address
+    }, (err, res) => {
+      t.error(err)
+      t.strictEqual(fastify.express.disabled('x-powered-by'), true)
+    })
+  })
+})
