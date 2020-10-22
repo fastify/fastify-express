402b52ccbf043d5103746e09a4bf38c3ecd81fd5
Simen Bekkhus <sbekkhus91@gmail.com>
2020-10-22 10:13:34 +0200

    fix: add `use` to types (#9)

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,13 @@
 import { Application } from 'express'
 import { FastifyPluginCallback } from 'fastify'
 
-declare module "fastify" {
+declare module 'fastify' {
   interface FastifyInstance {
+    /**
+     * Express middleware function
+     */
+    use: Application['use']
+
     /**
      * Express application instance
      */
