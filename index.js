e497274fad4e65cf9e4cd6b9cdf00c89bb87acc9
Simen Bekkhus <sbekkhus91@gmail.com>
2020-10-22 15:51:08 +0200

    chore: move header-setting inside condition

--- index.js
+++ index.js
@@ -52,11 +52,11 @@ function expressPlugin (fastify, options, next) {
   }
 
   function runConnect (req, reply, next) {
-    for (const [headerName, headerValue] of Object.entries(reply.getHeaders())) {
-      reply.raw.setHeader(headerName, headerValue)
-    }
-
     if (this[kMiddlewares].length > 0) {
+      for (const [headerName, headerValue] of Object.entries(reply.getHeaders())) {
+        reply.raw.setHeader(headerName, headerValue)
+      }
+
       this.express(req.raw, reply.raw, next)
     } else {
       next()
