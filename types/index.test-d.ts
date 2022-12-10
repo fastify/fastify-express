28102990ec41430362c1cd126bcbdee950348bb6
Ezekiel Keator <20361796+tastypackets@users.noreply.github.com>
2022-12-09 18:58:13 -0600

    Add tsd for Proxy

--- types/index.test-d.ts
+++ types/index.test-d.ts
@@ -1,19 +1,26 @@
-import Fastify from 'fastify'
+import Fastify, {FastifyRequest} from 'fastify'
 import fastifyExpress from '..'
 import { expectType } from "tsd"
 import { Application } from 'express'
 
 const app = Fastify()
 
+app.decorateRequest('testTsd', '')
 app.register(fastifyExpress)
 app.register(fastifyExpress, {
-  expressHook: 'onRequest'
+  expressHook: 'onRequest',
+  createProxyHandler: (fastifyReq) => ({
+    set(target, prop, value) {
+      expectType<FastifyRequest>(fastifyReq)
+      return Reflect.set(target, prop, value)
+    }
+  })
 })
 
 expectType<Application>(app.express)
 
 app.express.disable('x-powered-by')
 
-app.use('/world', (_req, res) => {
+app.use('/world', (req, res) => {
   res.sendStatus(200)
 })
